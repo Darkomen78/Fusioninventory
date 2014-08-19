d3060c6c1dda2cf99244fe81f17abfca4d2d856d
Darkomen <darkomen78@gmail.com>
2014-08-19 11:01:22 +0200

    Remove Perl full install
    Faster first time build !

--- Build_Fusioninventory_OSX_Flavor.sh
+++ Build_Fusioninventory_OSX_Flavor.sh
@@ -4,7 +4,7 @@
 FI_VERSION="2.3.10.1"
 FI_DIR="fusioninventory-agent-$FI_VERSION"
 # Perlbrew settings
-PERLBREWROOTDST=~/perl5
+PERLBREWROOTDST=$HOME/perl5
 OSXPERLVER="5.16.2"
 
 # Chemin d'installation pour la version OSX
@@ -12,7 +12,8 @@ INSTALL_PATH='/usr/local/fusioninventory'
 CONFDIR_PATH='/Library/Preferences/fusioninventory'
 DATADIR_PATH='/usr/local/fusioninventory/share'
 # Le répertoire pour les sources à packager
-SRCDST="`pwd`/Source"
+ROOTDIR="`pwd`"
+SRCDST=$ROOTDIR"/Source"
 
 export PERLBREW_ROOT=$PERLBREWROOTDST/perlbrew
 export PERLBREW_HOME=/tmp/.perlbrew
@@ -27,29 +28,21 @@ exit 0
 fi
 
 if [ ! -d $PERLBREWROOTDST ]; then
-clear
 echo "Perlbrew not found, install it"
 curl -L http://install.perlbrew.pl | bash
-echo "export PERLBREW_HOME=/tmp/.perlbrew" >> ~/.bash_profile
-echo "source $PERLBREWROOTDST/perlbrew/etc/bashrc" >> ~/.bash_profile
+echo 'export PERLBREW_HOME=/tmp/.perlbrew' >> ~/.bash_profile
+echo 'source $PERLBREWROOTDST/perlbrew/etc/bashrc' >> ~/.bash_profile
 read -p "Perlbrew install OK, please restart Terminal.app and relaunch this script"
 exit 0
 fi
 
-if [ ! -f $PERLBREWROOTDST/perlbrew/bin/cpanm ]; then
+if [[ ! -f $PERLBREWROOTDST/perlbrew/bin/cpanm ]]; then
 echo "cpanm in Perlbrew not found, install it"
 perlbrew install-cpanm
 fi
 
 $PERLBREWROOTDST/perlbrew/bin/cpanm --local-lib=$PERLBREWROOTDST local::lib && eval $(perl -I $PERLBREWROOTDST/lib/perl5/ -Mlocal::lib)
 
-if [[ ! -f $PERLBREWROOTDST/perlbrew/dists/perl-$OSXPERLVER.tar.bz2 ]]; then
-echo "OSX Perl current version ($OSXPERLVER) not found, install it"
-perlbrew install perl-$OSXPERLVER
-perlbrew switch perl-$OSXPERLVER
-fi
-
-perlbrew use $OSXPERLVER
 # Installe les modules manquants
 echo "install or update required Perl modules"
 cpanm -i Module::Install ExtUtils::MakeMaker HTTP::Proxy HTTP::Server::Simple HTTP::Server::Simple::Authen IO::Capture::Stderr IO::Socket::SSL IPC::Run JSON LWP::Protocol::https Net::SNMP POE::Component::Client::Ping Test::Compile Test::Deep Test::Exception Test::HTTP::Server::Simple Test::MockModule Test::MockObject Test::More Test::NoWarnings File::Remove File::Which LWP Net::IP Socket::GetAddrInfo Text::Template UNIVERSAL::require XML::TreePP
@@ -60,18 +53,30 @@ fi
 
 echo "Vide le dossier des sources"
 rm -Rf $SRCDST
-
 echo "Décompresse l'archive"
 tar xzf $FI_VERSION.tar.gz
 [ -d "$SRCDST" ] || mkdir "$SRCDST"
 cd $FI_DIR
 
-echo "Pseudo installation dans le dossier sources..."
-#perlbrew exec perl Makefile.PL PREFIX="$INSTALL_PATH" SYSCONFDIR="$CONFDIR_PATH" DATADIR="$DATADIR_PATH"
-perlbrew exec perl Makefile.PL SYSCONFDIR="$CONFDIR_PATH" DATADIR="$DATADIR_PATH"
-perlbrew exec make
-perlbrew exec make install PREFIX="$INSTALL_PATH" DESTDIR="$SRCDST"
+echo "Fake install in destination folder..."
+perl Makefile.PL SYSCONFDIR="$CONFDIR_PATH" DATADIR="$DATADIR_PATH"
+make
+make install PREFIX="$INSTALL_PATH" DESTDIR="$SRCDST"
 
-echo "Add perl dependance"
+echo "Add library dependances in the fak install"
 cpanm --installdeps -L extlib --notest .
 rsync -r extlib/lib/perl5/ "$SRCDST$DATADIR_PATH/lib/"
+
+echo "Rename default agent.cfg file to use later with OSX package postinstall script"
+mv $SRCDST$CONFDIR_PATH/agent.cfg $SRCDST$CONFDIR_PATH/agent.cfg.default
+#rm -Rf $ROOTDIR/$FI_DIR
+echo "######################################"
+echo "Modify agent.cfg.default"
+echo "######################################"
+echo "Add 127.0.0.1 in httpd-trust"
+sed -i "" "s/httpd-trust =/httpd-trust = 127.0.0.1/g" $SRCDST$CONFDIR_PATH/agent.cfg.default
+echo "######################################"
+echo "Change backend timeout from 30 to 180"
+sed -i "" "s/backend-collect-timeout = 30/backend-collect-timeout = 180/g" $SRCDST$CONFDIR_PATH/agent.cfg.default
+echo "######################################"
+
--- README.md
+++ README.md
@@ -4,3 +4,11 @@ Fusioninventory
 Everything you need to use and deploy FusionInventory-agent on OSX 
 
 More info at http://www.fusioninventory.org
+
+• Build_Fusioninventory_OSX_Flavor.sh
+
+Launch and follow script instructions.
+
+-> Install Perlbrew in your home folder (~/perl5)
+-> Download last version of FusionInventory-agent sources
+-> Create Source folder with files ready to copy or package
