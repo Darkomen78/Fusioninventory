79e43e0fde118b6452e062b0f3cdb3eeadbd3c03
tromatik <tromatik@gmail.com>
2015-05-08 19:49:02 +0200

    Fix agent sources archive check

--- Build_Fusioninventory_OSX_flavor.sh
+++ Build_Fusioninventory_OSX_flavor.sh
@@ -105,12 +105,12 @@ else
 	"$PERLBREWROOTDST/perlbrew/perls/perl-$OSXPERLVER/bin/cpanm" -i --force UNIVERSAL::require File::Which LWP Net::IP Text::Template UNIVERSAL::require XML::TreePP Compress::Zlib HTTP::Daemon IO::Socket::SSL Parse::EDID Proc::Daemon Proc::PID::File HTTP::Proxy HTTP::Server::Simple::Authen IPC::Run JSON Net::SNMP POE::Component::Client::Ping POSIX IO::Capture::Stderr LWP::Protocol::https Test::Compile Test::Deep Test::Exception Test::HTTP::Server::Simple Test::MockModule Test::MockObject Test::NoWarnings
 fi
 
-if [ ! -f /tmp/$FI_VERSION.tar.gz ]; then
-	cd /tmp/
-	curl -O -L $FUSIONSRC$FI_DIR.tar.gz && echo "Download $FI_DIR"
+if [ ! -f /tmp/$FI_DIR.tar.gz ]; then
+	curl -o /tmp/$FI_DIR.tar.gz -L $FUSIONSRC$FI_DIR.tar.gz && echo "Download $FI_DIR"
 fi
 
 echo "Empty destination folder"
+cd /tmp/
 rm -Rf $INSTALL_PATH/bin
 rm -Rf $INSTALL_PATH/share
 tar xzf $FI_DIR.tar.gz && rm $FI_DIR.tar.gz
