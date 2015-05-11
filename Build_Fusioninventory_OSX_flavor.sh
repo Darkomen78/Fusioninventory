72d7715a78344eb9ed234f5c32174e0eefe12920
Thomas Dannenmuller <tromatik@gmail.com>
2015-05-11 11:32:05 +0200

    Fix: add Socket::GetAddrInfo to fix agent 2.3.3
    Module required to launch 2.3.3 agent in daemon mode.

--- Build_Fusioninventory_OSX_flavor.sh
+++ Build_Fusioninventory_OSX_flavor.sh
@@ -107,7 +107,7 @@ echo
 if [[ $UPDMOD =~ ^[Nn]$ ]]; then
 	echo "...skip update modules"
 else
-	"$PERLBREWROOTDST/perlbrew/perls/perl-$OSXPERLVER/bin/cpanm" -i --force UNIVERSAL::require File::Which LWP Net::IP Text::Template UNIVERSAL::require XML::TreePP Compress::Zlib HTTP::Daemon IO::Socket::SSL Parse::EDID Proc::Daemon Proc::PID::File HTTP::Proxy HTTP::Server::Simple::Authen IPC::Run JSON Net::SNMP POE::Component::Client::Ping POSIX IO::Capture::Stderr LWP::Protocol::https Test::Compile Test::Deep Test::Exception Test::HTTP::Server::Simple Test::MockModule Test::MockObject Test::NoWarnings
+	"$PERLBREWROOTDST/perlbrew/perls/perl-$OSXPERLVER/bin/cpanm" -i --force Socket::GetAddrInfo UNIVERSAL::require File::Which LWP Net::IP Text::Template UNIVERSAL::require XML::TreePP Compress::Zlib HTTP::Daemon IO::Socket::SSL Parse::EDID Proc::Daemon Proc::PID::File HTTP::Proxy HTTP::Server::Simple::Authen IPC::Run JSON Net::SNMP POE::Component::Client::Ping POSIX IO::Capture::Stderr LWP::Protocol::https Test::Compile Test::Deep Test::Exception Test::HTTP::Server::Simple Test::MockModule Test::MockObject Test::NoWarnings
 fi
 
 if [ ! -f /tmp/$FI_DIR.tar.gz ]; then
