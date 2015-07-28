10adb9dbf96a981f719a98271801dddea6e0e95a
Darkomen <darkomen78@gmail.com>
2015-07-28 11:19:12 +0200

    Fix Brew install detection

--- Build_Fusioninventory_OSX_flavor.sh
+++ Build_Fusioninventory_OSX_flavor.sh
@@ -1,34 +1,39 @@
 #!/bin/bash
 
-# Version 1.2 by Sylvain La Gravière
+# Version 1.3.1 by Sylvain La Gravière
 # Twitter : @darkomen78
 # Mail : darkomen@me.com
 
-# Show help command if no version 
-: ${1?"this script need a version, type $0 help for more info"}
-
-# FusionInventory version
-FI_VERSION=$1 
-
-if [ $FI_VERSION = "help" ]; then
-	echo ""
-	echo "Usage: ""$0"" [version]"
-	echo "Example : ""$0"" 2.3.16"
-	echo ""
-exit 0
-fi
+# Version 1.3 by Thomas Dannenmüller
+# Mail : tromatik@gmail.com
 
 # Ask admin password
 (( EUID != 0 )) && exec sudo -- "$0" "$@"
 
+# FusionInventory version
+FI_VERSION=$1
+if [[ $FI_VERSION = [2-2].[0-0] || $FI_VERSION = [0-2].[0-3].[0-9] || $FI_VERSION = [0-2].[0-3].[0-1][0-6] ]]; then
+		echo "Building requested package version: $FI_VERSION"
+	else
+		echo -e "\nUsage: ""$0"" [version]\nExample : ""$0"" 2.3.16\nAvailable versions (pre 2.3.5) : https://cpan.metacpan.org/authors/id/G/GO/GONERI/\nAvailable versions (post 2.3.5) : https://cpan.metacpan.org/authors/id/G/GR/GROUSSE/\n"
+		exit 0
+fi
+
 # Source base URL
-FUSIONSRC="https://cpan.metacpan.org/authors/id/G/GR/GROUSSE/"
+TEST_VERSION=$(echo $FI_VERSION | sed  s'/\.//g')
+if (( $TEST_VERSION >= 236 )); then
+	echo "Fetching source package from https://cpan.metacpan.org/authors/id/G/GR/GROUSSE/ (post 2.3.5)"
+	FUSIONSRC="https://cpan.metacpan.org/authors/id/G/GR/GROUSSE/"
+	else if (( $TEST_VERSION <= 236 )); then
+		echo "Fetching source package from https://cpan.metacpan.org/authors/id/G/GO/GONERI/ (pre 2.3.5)"
+		FUSIONSRC="https://cpan.metacpan.org/authors/id/G/GO/GONERI/"
+	fi
+fi
 PACKAGESSRC="http://s.sudre.free.fr/Software/files/Packages.dmg"
 GITSRC="https://raw.github.com/Darkomen78/Fusioninventory/master/source/"
 
-# Perl Version : Lion 5.12.3 - Mountain Lion 5.12.4 - Maverick 5.16.2 - Yosemite 5.18.2
-# Install this version in perlbrew, must work on 10.8+
-OSXPERLVER=5.16.2
+# Automatic Perl version detection
+OSXPERLVER=$(perl -v | grep v5 | cut -d "(" -f 2 | cut -d ")" -f 1 | sed s'/v//'g)
 
 # Temporary local source folder
 FI_DIR="FusionInventory-Agent-$FI_VERSION"
@@ -41,9 +46,11 @@ DEPLOYPROJ="FusionInventory_deploy.pkgproj"
 INSTALL_PATH='/usr/local/fusioninventory'
 CONFDIR_PATH='/Library/Preferences/fusioninventory'
 DATADIR_PATH='/usr/local/fusioninventory/share'
+
 # Current dir
 ROOTDIR="`pwd`"
-# Local final folder 
+
+# Local final folder
 SRCDST="$ROOTDIR/$FI_VERSION/"
 
 
@@ -57,7 +64,7 @@ if [ -f $PERLBREW_ROOT/etc/bashrc ]; then
 fi
 
 if [ ! -d /Library/Developer/CommandLineTools ]; then
-	clear
+	#clear
 	echo "Xcode command line tools not found, install it..."
 	xcode-select --install
 	read -p "When Xcode command line tools install is finish, please relaunch this script" -t 5
@@ -65,8 +72,8 @@ if [ ! -d /Library/Developer/CommandLineTools ]; then
 	exit 0
 fi
 
-if [ ! -d $PERLBREWROOTDST ]; then
-	clear
+if [ ! -d $PERLBREW_ROOT ]; then
+	#clear
 	echo "Perlbrew not found, install it..."
 	curl -L 'http://install.perlbrew.pl' | bash
 	read -p "Perlbrew install is OK. Quit and restart Terminal, then relaunch this script" -t 5
@@ -75,22 +82,22 @@ if [ ! -d $PERLBREWROOTDST ]; then
 fi
 
 if [ ! -d "$PERLBREW_ROOT"/perls/perl-"$OSXPERLVER" ]; then
-	clear
+	#clear
 	echo "Perl $OSXPERLVER in Perlbrew not found, install it..."
-	perlbrew install perl-$OSXPERLVER
+	perlbrew install perl-$OSXPERLVER -D usethreads
 	read -p "Perl $OSXPERLVER install is finish, please relaunch this script" -t 5
 	echo
 	exit 0
 fi
 
 if [ -d $PERLBREWROOTDST/perlbrew/perls/perl-$OSXPERLVER ]; then
-	clear
+	#clear
 	echo "################## Switch to Perl version $OSXPERLVER #######################"
 	perlbrew switch "$OSXPERLVER"
 fi
 
 if [ ! -f $PERLBREWROOTDST/perlbrew/perls/perl-$OSXPERLVER/bin/cpanm ]; then
-	clear
+	#clear
 	echo "cpanm in Perlbrew not found, install it..."
 	cpan -i App::cpanminus
 fi
@@ -100,23 +107,22 @@ echo
 if [[ $UPDMOD =~ ^[Nn]$ ]]; then
 	echo "...skip update modules"
 else
-	"$PERLBREWROOTDST/perlbrew/perls/perl-$OSXPERLVER/bin/cpanm" -i --force File::Which LWP Net::IP Text::Template UNIVERSAL::require XML::TreePP Compress::Zlib HTTP::Daemon IO::Socket::SSL Parse::EDID Proc::Daemon Proc::PID::File HTTP::Proxy HTTP::Server::Simple::Authen IPC::Run JSON Net::SNMP POE::Component::Client::Ping POSIX IO::Capture::Stderr LWP::Protocol::https Test::Compile Test::Deep Test::Exception Test::HTTP::Server::Simple Test::MockModule Test::MockObject Test::NoWarnings File::Copy::Recursive
-
+	"$PERLBREWROOTDST/perlbrew/perls/perl-$OSXPERLVER/bin/cpanm" -i --force File::Which LWP Net::IP Text::Template UNIVERSAL::require XML::TreePP Compress::Zlib HTTP::Daemon IO::Socket::SSL Parse::EDID Proc::Daemon Proc::PID::File HTTP::Proxy HTTP::Server::Simple::Authen IPC::Run JSON Net::SNMP POE::Component::Client::Ping POSIX IO::Capture::Stderr LWP::Protocol::https Test::Compile Test::Deep Test::Exception Test::HTTP::Server::Simple Test::MockModule Test::MockObject Test::NoWarnings File::Copy::Recursive Socket::GetAddrInfo UNIVERSAL::require
 fi
 
-if [ ! -f /tmp/$FI_VERSION.tar.gz ]; then
-	cd /tmp/
-	curl -O -L $FUSIONSRC$FI_DIR.tar.gz && echo "Download $FI_DIR"
+if [ ! -f /tmp/$FI_DIR.tar.gz ]; then
+	curl -o /tmp/$FI_DIR.tar.gz -L $FUSIONSRC$FI_DIR.tar.gz && echo "Download $FI_DIR"
 fi
 
 echo "Empty destination folder"
+cd /tmp/
 rm -Rf $INSTALL_PATH/bin
 rm -Rf $INSTALL_PATH/share
 tar xzf $FI_DIR.tar.gz && rm $FI_DIR.tar.gz
 cd /tmp/$FI_DIR
 
 echo "Temporary install..."
-export SYSCONFDIR="$CONFDIR_PATH" 
+export SYSCONFDIR="$CONFDIR_PATH"
 export DATADIR="$DATADIR_PATH"
 perl Makefile.PL -I lib SYSCONFDIR="$CONFDIR_PATH" DATADIR="$DATADIR_PATH"
 make
@@ -134,11 +140,17 @@ echo "######################################"
 echo "Change backend timeout from 30 to 180"
 sed -i "" "s/backend-collect-timeout = 30/backend-collect-timeout = 180/g" $CONFDIR_PATH/agent.cfg.default
 echo "######################################"
+ if (( $TEST_VERSION <= 238 )); then
+	echo "######################################"
+	echo "Comment scan-profiles option for pre 2.3.8 versions of the agent"
+	sed -i "" "s/scan-profiles = 0/#scan-profiles = 0/g" $CONFDIR_PATH/agent.cfg.default
+	echo "######################################"
+fi
 
 echo "Move files to Source folder for packages..."
 if [ ! -d "$SRCDST/Source" ]; then
 	mkdir -p "$SRCDST/Source"
-else 
+else
 	read -p "----------------> Use current source folder ? [Y] " -n 1 -r USECURRENTSRC
 	if [[ $USECURRENTSRC =~ ^[Nn]$ ]]; then
 		if [ -d "$SRCDST/Source_previous" ]; then
@@ -176,7 +188,7 @@ read -p "----------------> Create standard package ? [Y] " -n 1 -r PKG
 echo
 if [[ $PKG =~ ^[Nn]$ ]]; then
 	echo "...skip create standard package"
-else	
+else
 	if [ ! -d "/Applications/Packages.app" ]; then
 		echo "No Packages install found, install it..."
 		cd /tmp/
@@ -184,7 +196,7 @@ else
 		hdiutil mount /tmp/Packages.dmg && echo "Mount Packages install"
 		/usr/sbin/installer -dumplog -verbose -pkg "/Volumes/Packages/packages/Packages.pkg" -target / && echo "Install Packages" && hdiutil unmount /Volumes/Packages/ && echo "Unmount Packages install"
 	fi
-	if [ ! -f "$SRCDST/FusionInventory.pkgproj" ]; then	
+	if [ ! -f "$SRCDST/FusionInventory.pkgproj" ]; then
 		echo "FusionInventory.pkgproj not found, download it..."
 		cd "$SRCDST"
 		curl -O -L "$GITSRC$PROJ"
@@ -201,7 +213,7 @@ if [[ $DEPLOY =~ ^[Nn]$ ]]; then
 	echo "...skip create deployment package"
 	echo
 	exit 0
-else	
+else
 	if [ ! -d "/Applications/Packages.app" ]; then
 		echo "No Packages install found, install it..."
 		cd /tmp/
@@ -209,7 +221,7 @@ else
 		hdiutil mount /tmp/Packages.dmg && echo "Mount Packages install"
 		/usr/sbin/installer -dumplog -verbose -pkg "/Volumes/Packages/packages/Packages.pkg" -target / && echo "Install Packages" && hdiutil unmount /Volumes/Packages/ && echo "Unmount Packages install"
 	fi
-	if [ ! -f "$SRCDST/FusionInventory_deploy.pkgproj" ]; then	
+	if [ ! -f "$SRCDST/FusionInventory_deploy.pkgproj" ]; then
 		echo "FusionInventory_deploy.pkgproj not found, download it..."
 		cd "$SRCDST"
 		curl -O -L "$GITSRC$DEPLOYPROJ"
@@ -233,11 +245,11 @@ else
 	echo
 	if [[ $CONF =~ ^[Nn]$ ]]; then
 		echo "...skip configure deployment package"
-		echo	
+		echo
 		exit 0
 	else
-		open "$SRCDST/Deploy/Configure.command"	
+		open "$SRCDST/Deploy/Configure.command"
 	fi
 fi
 echo
-exit 0	
+exit 0
